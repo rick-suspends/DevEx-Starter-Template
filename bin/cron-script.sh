6013d73f0449ffedafb5e15e084e9bd653af3fed
Rick Mallery <richard_mallery@yahoo.com>
2026-01-20 12:18:07 -0700

    more updates for cron

---
+++ bin/cron-script.sh
@@ -0,0 +1,6 @@
+date
+curl https://webservices.launchpad-dev.vianai.site/v1/microstack/renew/dev-1759153831?action=START
+echo
+curl https://webservices.launchpad-dev.vianai.site/v1/microstack/renew/dev-1759153831?delete=False
+echo
+echo
